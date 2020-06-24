62a10454a2031b6afe7e915d578b115617dfc3c5
Pyoncakes <fabian.schiller2002@gmail.com>
2020-06-24 17:57:04 +0200

    Creating a logic class. Connecting buttons to a function in said class.

--- logic.py
+++ logic.py
@@ -1,1 +1,13 @@
 """Handles all the calculations for the calculator."""
+
+
+class Logic:
+    def __init__(self, calc, display):
+        self.calc = calc
+        self.display = display
+        self.input_num = None
+        self.stored = None
+        self.operator = None
+
+    def button_press(self, button):
+        print(button.text())
